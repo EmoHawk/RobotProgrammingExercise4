2631f1d55d99e5ea34c7f209977549ef404e6d50
EmoHawk <bobalob123@hotmail.com>
2014-03-04 10:05:06 +0000

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,29 @@
+
+public class Switch 
+{
+	private int switchOne;
+	private int switchTwo;
+	
+	public Switch(int switchOne, int switchTwo) 
+	{
+		this.switchOne = switchOne;
+		this.switchTwo = switchTwo;
+	}
+
+	public int getSwitchOne() {
+		return switchOne;
+	}
+
+	public void setSwitchOne(int switchOne) {
+		this.switchOne = switchOne;
+	}
+
+	public int getSwitchTwo() {
+		return switchTwo;
+	}
+
+	public void setSwitchTwo(int switchTwo) {
+		this.switchTwo = switchTwo;
+	}
+	
+}
